f9b2de6b9a2fe54db5eaa723703308009243b2db
Vanster78 <itsmevanster@gmail.com>
2020-09-20 20:50:43 +0300

    Implement double classification functions

--- classify double/solution.c
+++ classify double/solution.c
@@ -3,7 +3,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-
+const uint64_t PLUS_ZERO = 0x0000000000000000;
+const uint64_t MINUS_ZERO = 0x8000000000000000;
+const uint64_t MAX_EXPONENT = 0x7FF0000000000000;
+const uint64_t MAX_FRACTION = 0x000FFFFFFFFFFFFF;
 
 /**
  * Library-level functions.
@@ -15,56 +18,84 @@ uint64_t convertToUint64 (double number) {
 }
 
 bool getBit (const uint64_t number, const uint8_t index) {
-    /// Your code here...
+    return (number >> index) & 1u;
 }
 
+bool checkForPositive(const uint64_t number) {
+    return !getBit(number, 63);
+}
+
+bool checkForSingleValueExponent(const uint64_t number, const bool value) {
+    return (number & MAX_EXPONENT) == (value ? MAX_EXPONENT : PLUS_ZERO);
+}
+
+bool checkForSingleValueFraction(const uint64_t number, const bool value) {
+    return (number & MAX_FRACTION) == (value ? MAX_FRACTION : PLUS_ZERO);
+}
 
 /**
  * Checkers here:
  */
 
 bool checkForPlusZero (uint64_t number) {
-    /// Your code here.
+    return number == PLUS_ZERO;
 }
 
 bool checkForMinusZero (uint64_t number) {
-    return number == 0x8000000000000000;
+    return number == MINUS_ZERO;
 }
 
 bool checkForPlusInf (uint64_t number) {
-    /// Your code here.
+    return checkForPositive(number) &&
+           checkForSingleValueExponent(number, 1) &&
+           checkForSingleValueFraction(number, 0);
 }
 
 bool checkForMinusInf (uint64_t number) {
-    /// Your code here.
+    return !checkForPositive(number) &&
+           checkForSingleValueExponent(number, 1) &&
+           checkForSingleValueFraction(number, 0);
 }
 
 bool checkForPlusNormal (uint64_t number) {
-    /// Your code here.
+    return checkForPositive(number) &&
+           !checkForSingleValueExponent(number, 1) &&
+           !checkForSingleValueExponent(number, 0);
 }
 
 bool checkForMinusNormal (uint64_t number) {
-    /// Your code here.
+    return !checkForPositive(number) &&
+           !checkForSingleValueExponent(number, 1) &&
+           !checkForSingleValueExponent(number, 0);
 }
 
 bool checkForPlusDenormal (uint64_t number) {
-    /// Your code here.
+    return checkForPositive(number) &&
+           checkForSingleValueExponent(number, 0) &&
+           !checkForSingleValueFraction(number, 0);
 }
 
 bool checkForMinusDenormal (uint64_t number) {
-    /// Your code here.
+    return !checkForPositive(number) &&
+           checkForSingleValueExponent(number, 0) &&
+           !checkForSingleValueFraction(number, 0);
 }
 
 bool checkForSignalingNan (uint64_t number) {
-    /// Your code here.
+    return checkForSingleValueExponent(number, 1) &&
+           !checkForSingleValueFraction(number, 0) &&
+           !getBit(number, 51);
 }
 
 bool checkForQuietNan (uint64_t number) {
-    /// Your code here.
+    return checkForSingleValueExponent(number, 1) &&
+           getBit(number, 51);
 }
 
-
 void classify (double number) {
+    const uint64_t a = convertToUint64(number);
+    for (int i = 63; i >= 0; --i)
+        printf("%d", getBit(a, i));
     if (checkForPlusZero(convertToUint64(number))) {
         printf("Plus zero\n");
     }
